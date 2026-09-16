49d6385bd0fbb998a65e9c133ba5acf25e1413b8
leyagross123-tech <leyagross123@gmail.com>
2026-09-16 22:09:44 +0100

    Update linguistic_functions.py

--- linguistic_functions.py
+++ linguistic_functions.py
@@ -197,3 +197,23 @@ def split_blocks(text, number):
         block = ''
         starter = starter + 1
     return blocks
+
+def gen(key, mode='', alph=ALPHABET):
+    # generates an alphabet key from a keyword
+    # choose mode L to generate a key appending the alphabet from the last letter of the keyword
+    temp = ''
+    for letter in key:
+        if letter not in temp:
+            temp = temp + letter
+        key = temp
+    if mode != 'L': #L = last letter mode
+        for char in ALPHABET:
+            if char not in key:
+                key = key + char
+    else:
+        temp = key + alph[alph.index(key[-1])+1:] + alph[:alph.index(key[-1])]
+        key = ''
+        for char in temp:
+            if char not in key:
+                key = key + char
+    return key
